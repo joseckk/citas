0cb4e9c4e91a99503f253e17a681a0664aa7823d
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-27 19:02:57 +0100

    base de datos corregida

--- comunes/bdCitas.sql
+++ comunes/bdCitas.sql
@@ -18,18 +18,17 @@ DROP TABLE IF EXISTS citas CASCADE;
 CREATE TABLE citas
 (
         id          bigserial        PRIMARY KEY
-    ,   hora        timestamp        NOT NULL UNIQUE
-    ,   duracion    numeric(2)       NOT NULL
-    ,   usuario_id  bigint           REFERENCES usuarios (id)
+    ,   fecha_hora  timestamp(0)     NOT NULL UNIQUE
+    ,   usuario_id  bigint           NOT NULL REFERENCES usuarios (id)
 );
+
+CREATE INDEX idx_citas_usuario_id ON citas (usuario_id);
 
 INSERT INTO usuarios (login, password)
 VALUES ('admin', crypt('admin', gen_salt('bf', 10)))
      , ('pepe', crypt('pepe', gen_salt('bf', 10)))
      , ('manolo', crypt('manolo', gen_salt('bf', 10)));
 
-INSERT INTO citas (hora, duracion, usuario_id)
-VALUES ('2015-06-02 18:23:15', 15, 2)
-     , ('2015-06-20 16:23:15', 15, 3)
-     , ('2020-09-25 17:23:15', 15, NULL)
-     , ('2018-07-02 19:23:15', 15, NULL);
+INSERT INTO citas (fecha_hora, usuario_id)
+VALUES ('2015-06-02 18:23:15', 2)
+     , ('2015-06-20 16:23:15', 3);
